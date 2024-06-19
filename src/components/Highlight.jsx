635a5cbb1a8b45485cb8bcea46dd6aee530fd641
kudawoo2002 <kudawoo2002@gmail.com>
2024-06-19 07:33:14 +0000

    Adding hero

--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -1,5 +1,32 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 function Highlight() {
-  return <div>Highlight</div>;
+  useGSAP(() => {
+    gsap.to(
+      "#title",
+      {
+        opacity: 1,
+        y: -40,
+        delay: 3,
+      },
+      []
+    );
+  });
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Highlight;
